9b5310850d567c7340d018b082139b7512ca2673
MUTHARASU A <mutharasu39@gmail.com>
2023-11-15 11:12:05 +0530

    templates updated

--- templateproj/temp_project/views.py
+++ templateproj/temp_project/views.py
@@ -3,5 +3,5 @@ import datetime
 # Create your views here.
 def html_file_rendering(request):
     date = datetime.datetime.now()
-    date_dict :{'datetim':date}
+    date_dict ={'datetim':date}
     return render(request,'temp_projects/temp.html',context=date_dict)
