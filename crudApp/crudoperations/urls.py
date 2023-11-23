8935c55230cb4c1d2607d7fe755f0f5bf461a678
MUTHARASU A <mutharasu39@gmail.com>
2023-11-23 13:04:33 +0530

    updated final crud operation on django

--- crudApp/crudoperations/urls.py
+++ crudApp/crudoperations/urls.py
@@ -3,5 +3,7 @@ from crudoperations import views
 urlpatterns = [
     path('check/',views.studtent_retrive,name='crudoperations'),
     path('add/',views.add_student,name='add_student'),
+    path('delete/<int:id>/',views.delete_student,name='delete'),
+    path('update/<int:id>/',views.update_student,name='update'),
 
 ]
--- crudApp/crudoperations/views.py
+++ crudApp/crudoperations/views.py
@@ -16,3 +16,17 @@ def add_student(request):
             form.save()
             return redirect('/check')
     return render(request,'crudoperations/forms.html',{"form":form})
+
+def delete_student(request,id):
+    stud = student.objects.get(id = id)
+    stud.delete()
+    return redirect('/check')
+def update_student(request,id):
+    stud = student.objects.get(id = id)
+    form = studentForm(request.POST,instance = stud)
+ 
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect('/check')
+    return render(request,'crudoperations/update.html',{'stud':stud})
